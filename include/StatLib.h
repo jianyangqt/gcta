0efaf5e61d021e1d6112d6b870b253c03c5aa0be
zhilizheng <zhilizheng@outlook.com>
2017-12-21 09:58:45 +1000

    add statlic header

--- include/StatLib.h
+++ include/StatLib.h
@@ -1,3 +1,19 @@
+/*
+   GCTA: a tool for Genome-wide Complex Trait Analysis
+
+   Some statistic functions
+
+   Developed by Zhili Zheng<zhilizheng@outlook.com>
+
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   A copy of the GNU General Public License is attached along with this program.
+   If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef STAT_LIB_H
 #define STAT_LIB_H
 namespace StatLib{
