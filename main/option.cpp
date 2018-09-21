b799d65e1ab4df059b37ede810b1e5492250d025
ZhiliZheng <zhilizheng@outlook.com>
2018-09-21 15:08:01 +1000

    change flag of mlma no adj covar

--- main/option.cpp
+++ main/option.cpp
@@ -897,8 +897,11 @@ void option(int option_num, char* option_str[])
             LOGGER << "--mlma-loco " << endl;
         } else if (strcmp(argv[i], "--mlma-no-adj-covar") == 0) {
             mlma_no_adj_covar = true;
-            LOGGER << "--mlma-no-adj-covar " << endl;
-        } else if (strcmp(argv[i], "--fst") == 0) {
+            LOGGER << "--mlma-no-adj-covar (use --mlma-no-preadj-covar instead)" << endl;
+        } else if (strcmp(argv[i], "--mlma-no-preadj-covar") == 0) {
+            mlma_no_adj_covar = true;
+            LOGGER << "--mlma-no-preadj-covar " << endl;
+        }else if (strcmp(argv[i], "--fst") == 0) {
             fst_flag = true;
             LOGGER << "--fst " << endl;
         } else if (strcmp(argv[i], "--sub-popu") == 0) {
